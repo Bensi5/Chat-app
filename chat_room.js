43729cefe67304b1cb544602acb9f6d8a6c361b4
Bensi5 <77843600+Bensi5@users.noreply.github.com>
2021-07-02 18:52:55 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD4pEecKZkUrdTz5xG0OgY1K-Cq6s8lkXA",
+    authDomain: "kwitter-app-82002.firebaseapp.com",
+    databaseURL: "https://kwitter-app-82002-default-rtdb.firebaseio.com",
+    projectId: "kwitter-app-82002",
+    storageBucket: "kwitter-app-82002.appspot.com",
+    messagingSenderId: "832928017656",
+    appId: "1:832928017656:web:c44273e3b39199b7921f0d"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
